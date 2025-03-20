143c37c25a81f6b7f2b377f60da5af347f966251
Reeshav Acharya <reeshav99acharya@gmail.com>
2025-03-20 13:08:31 +0545

    Only accept stake address of hex length 58

--- src/controllers/stakeAddress.ts
+++ src/controllers/stakeAddress.ts
@@ -1,22 +1,24 @@
-import { Request, Response, Router } from "express";
-import { handlerWrapper } from "../errors/AppError";
-import { convertToHexIfBech32, validateAddress } from "../helpers/validator";
-import { fetchStakeAddressDetails } from "../repository/stakeAddress";
+import { Request, Response, Router } from 'express'
+import { handlerWrapper } from '../errors/AppError'
+import { convertToHexIfBech32, validateAddress } from '../helpers/validator'
+import { fetchStakeAddressDetails } from '../repository/stakeAddress'
 
-const router = Router();
+const router = Router()
 
 const getStakeAddressDetails = async (req: Request, res: Response): Promise<any> => {
-  let address = convertToHexIfBech32(req.query.address as string)
-  if (validateAddress(address)){
-    address = address.length === 56 ? `e0${address}`:address
-  }else{
-    return res.status(400).json({message:'Provide a valid address'})
-  }
+    let address = convertToHexIfBech32(req.query.address as string)
+    if (validateAddress(address)) {
+        if (address.length == 56) {
+            return res.status(400).json({ mesage: `Provide a valid address` })
+        }
+    } else {
+        return res.status(400).json({ message: 'Provide a valid address' })
+    }
 
-const result = await fetchStakeAddressDetails(address)
-  return res.status(200).json(result);
-};
+    const result = await fetchStakeAddressDetails(address)
+    return res.status(200).json(result)
+}
 
-router.get('/', handlerWrapper(getStakeAddressDetails));
+router.get('/', handlerWrapper(getStakeAddressDetails))
 
-export default router;
+export default router
